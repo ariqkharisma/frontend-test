ccf214968a3329d05214306c6ba86190aa3d7361
ariqkharisma <ariqkharisma10@gmail.com>
2022-11-21 06:26:34 +0700

    update

---
+++ news-webapp/src/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div style={{width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <h1>Page Not Found</h1>
+    </div>
+  )
+}
+
+export default NotFound
